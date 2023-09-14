c6b42ba9228673092216bfab958d2bbd247f2901
andresNumpaque11 <andres.suarez07@uptc.edu.co>
2023-09-14 17:32:32 -0500

    cambio 2
    cambiar el codigo añadiendo otro mensaje :)

--- andresNumpaque11/src/model/HolaMundo.java
+++ andresNumpaque11/src/model/HolaMundo.java
@@ -3,6 +3,7 @@ package model;
 public class HolaMundo{
 	public static void public static void main(String[] args) {
 		System.out.println("Hola Soy Andres Numpaque");
-		
+		System.out.println("cambio 2");
+
 	}
 }
